3eee55029af4cc8295ff8bdcdc8efb908fe2e3e2
chamindaindunil <chaminda.indunil@hotmail.com>
2021-05-23 14:35:10 +0530

    remove chunk for loop

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,7 @@ var Order = require('../models/order');
 router.get('/', function(req, res, next) {
   var successMsg = req.flash('success')[0];
   Product.find(function(err, docs) {
-    var productChunks = [];
-    var chunkSize = 3;
-    for (var i = 0; i < docs.length; i += chunkSize) {
-      productChunks.push(docs.slice(i, i + chunkSize));
-    }
-    res.render('shop/index', { title: 'Shopping Cart', products: productChunks, successMsg: successMsg, noMessages: !successMsg });
+    res.render('shop/index', { title: 'Shopping Cart', products: docs, successMsg: successMsg, noMessages: !successMsg });
   }).lean();
 });
 
