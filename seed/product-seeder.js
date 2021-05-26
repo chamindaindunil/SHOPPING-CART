f8f10b86fb6e4ab6d528a633498dd6cd2833cf27
chamindaindunil <chaminda.indunil@hotmail.com>
2021-05-26 22:21:30 +0530

    try to resolve decimal price issue

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -10,31 +10,31 @@ var products = [
         imagePath: 'https://gidofgames.com/uploads/posts/2017-10/1507925155_poster-gothic-2.jpg',
         title: 'Gothic Video Game',
         description: 'Awesome Game !!!!!!!',
-        price: 710
+        price: 710.00
     }),
     new Product({
         imagePath: 'https://images-na.ssl-images-amazon.com/images/I/716mcbaBxIL._SL1151_.jpg',
         title: 'Minecraft Video Game',
         description: 'Now that is super awesome!',
-        price: 1500
+        price: 1500.00
     }),
     new Product({
         imagePath: 'https://cdn-prod.scalefast.com/public/assets/user/122595/image/5243deb55a7ee9587ad2db9298e96ca5.jpg',
         title: 'Dark Souls 3 Video Game',
         description: 'I died!',
-        price: 560
+        price: 560.00
     }),
     new Product({
         imagePath: 'https://cdn-products.eneba.com/resized-products/dEdZeooTmlzeNGBKPTKCbL7Ob_wvjzO4sNYvr1f-xYU_350x200_1x-0.jpg',
         title: 'Free Fire Video Game',
         description: 'I Shout!',
-        price: 1600
+        price: 1600.00
     }),
     new Product({
         imagePath: 'https://upload.wikimedia.org/wikipedia/en/f/fd/Call_of_Duty_-_Infinite_Warfare_%28promo_image%29.jpg',
         title: 'Call of Duty Video Game',
         description: 'Infinite Warfare underperformed!',
-        price: 620
+        price: 620.00
     }),
 ];
 
